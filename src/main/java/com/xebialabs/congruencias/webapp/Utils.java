c69ce2a0b54d72e20862d700862f2f26adfe4337
José Carlos López Ayala <josecarlos.lopezayala@gmail.com>
2018-09-20 00:23:32 +0200

    nuevo color

--- src/main/java/com/xebialabs/congruencias/webapp/Utils.java
+++ src/main/java/com/xebialabs/congruencias/webapp/Utils.java
@@ -32,10 +32,10 @@ public class Utils {
 
 	public static String getCng000() {
 		String template = getHeader();
-		//Comentario
+		//Comentarios
 		template = template + "    <form class=\"form-signin\" name=\"form01\" method=\"post\" action=\"validaUsuario\">";
 		template = template + "      <h1 class=\"h3 mb-3 font-weight-normal\">";
-		template = template + "      <font color=\"red\">Bienvenido a la calculadora matemática</font>";
+		template = template + "      <font color=\"purple\">Bienvenido a la calculadora matemática</font>";
 		template = template + "      </h1>";
 		template = template + "      <img class=\"mb-4\" src=\"images/com.calc.scientific.png\" alt=\"\" width=\"92\" height=\"92\">";
 		template = template + "      <h1 class=\"h3 mb-3 font-weight-normal\"></h1>";
